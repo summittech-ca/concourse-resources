332ae032e45b4358c0b1a8ced1814e7dbd0eb0f3
Eric Bellotti <ericb@summit-tech.ca>
2023-01-02 20:18:44 +0000

    Fix build script

--- gerrit/build.sh
+++ gerrit/build.sh
@@ -2,6 +2,12 @@
 
 readonly __SCRIPT_DIR="$(echo $(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && cd .. && pwd))"
 
+pushd ${__SCRIPT_DIR}
+go mod init github.com/google/concourse-resources || :
+go mod tidy
+go mod vendor
+popd
+
 mkdir -p ${GOPATH}/src/github.com/google/
 rm -f ${GOPATH}/src/github.com/google/concourse-resources
 ln -s ${__SCRIPT_DIR}/ ${GOPATH}/src/github.com/google/concourse-resources
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/google/concourse-resources
+
+go 1.18
+
+require (
+	github.com/stretchr/testify v1.1.4
+	golang.org/x/build v0.0.0-20170811201810-5d60a938406c
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
